e0910085f37ad4fddb9a5cb53898979ecb002d77
Giovanna Revito <giovannarev.roz@gmail.com>
2025-05-02 09:00:51 -0300

    commit8

--- src/main/java/br/com/fiap/sprint1/dto/CargoDTO.java
+++ src/main/java/br/com/fiap/sprint1/dto/CargoDTO.java
@@ -13,4 +13,36 @@ public class CargoDTO extends RepresentationModel<CargoDTO> {
         private String nomeCargo;
         private String descricaoCargo;
         private List<Funcionario> funcionarios;
+
+        public Long getIdCargo() {
+                return idCargo;
+        }
+
+        public void setIdCargo(Long idCargo) {
+                this.idCargo = idCargo;
+        }
+
+        public String getNomeCargo() {
+                return nomeCargo;
+        }
+
+        public void setNomeCargo(String nomeCargo) {
+                this.nomeCargo = nomeCargo;
+        }
+
+        public String getDescricaoCargo() {
+                return descricaoCargo;
+        }
+
+        public void setDescricaoCargo(String descricaoCargo) {
+                this.descricaoCargo = descricaoCargo;
+        }
+
+        public List<Funcionario> getFuncionarios() {
+                return funcionarios;
+        }
+
+        public void setFuncionarios(List<Funcionario> funcionarios) {
+                this.funcionarios = funcionarios;
+        }
 }
--- src/main/java/br/com/fiap/sprint1/dto/ClienteDTO.java
+++ src/main/java/br/com/fiap/sprint1/dto/ClienteDTO.java
@@ -16,4 +16,60 @@ public class ClienteDTO extends RepresentationModel<ClienteDTO> {
         private String emailCliente;
         private String sexoCliente;
         private List<Moto> motos;
+
+        public Long getIdCliente() {
+                return idCliente;
+        }
+
+        public void setIdCliente(Long idCliente) {
+                this.idCliente = idCliente;
+        }
+
+        public String getNomeCliente() {
+                return nomeCliente;
+        }
+
+        public void setNomeCliente(String nomeCliente) {
+                this.nomeCliente = nomeCliente;
+        }
+
+        public String getTelefoneCliente() {
+                return telefoneCliente;
+        }
+
+        public void setTelefoneCliente(String telefoneCliente) {
+                this.telefoneCliente = telefoneCliente;
+        }
+
+        public String getCpfCliente() {
+                return cpfCliente;
+        }
+
+        public void setCpfCliente(String cpfCliente) {
+                this.cpfCliente = cpfCliente;
+        }
+
+        public String getEmailCliente() {
+                return emailCliente;
+        }
+
+        public void setEmailCliente(String emailCliente) {
+                this.emailCliente = emailCliente;
+        }
+
+        public String getSexoCliente() {
+                return sexoCliente;
+        }
+
+        public void setSexoCliente(String sexoCliente) {
+                this.sexoCliente = sexoCliente;
+        }
+
+        public List<Moto> getMotos() {
+                return motos;
+        }
+
+        public void setMotos(List<Moto> motos) {
+                this.motos = motos;
+        }
 }
