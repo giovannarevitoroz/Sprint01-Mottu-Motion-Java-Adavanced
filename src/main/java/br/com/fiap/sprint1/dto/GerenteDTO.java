583e10b6195249b5f76835071f0e9a8943113e25
Giovanna Revito <giovannarev.roz@gmail.com>
2025-05-02 08:56:22 -0300

    commit6

--- src/main/java/br/com/fiap/sprint1/dto/GerenteDTO.java
+++ src/main/java/br/com/fiap/sprint1/dto/GerenteDTO.java
@@ -1,22 +1,55 @@
 package br.com.fiap.sprint1.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import br.com.fiap.sprint1.model.Patio;
+import org.springframework.hateoas.RepresentationModel;
 
-public record GerenteDTO(
-        Long idGerente,
+public class GerenteDTO extends RepresentationModel<GerenteDTO> {
 
-        @NotBlank(message = "O nome do gerente não pode estar em branco.")
-        @Size(max = 100, message = "O nome do gerente deve ter no máximo 100 caracteres.")
-        String nomeGerente,
+        private Long idGerente;
+        private String nomeGerente;
+        private String telefoneGerente;
+        private String cpfGerente;
+        private Patio patio;
 
-        @NotBlank(message = "O telefone do gerente não pode estar em branco.")
-        @Size(max = 11, message = "O telefone do gerente deve ter no máximo 11 caracteres.")
-        String telefoneGerente,
+        // Getters e Setters
 
-        @NotBlank(message = "O CPF do gerente não pode estar em branco.")
-        @Size(max = 11, message = "O CPF do gerente deve ter no máximo 11 caracteres.")
-        String cpfGerente,
+        public Long getIdGerente() {
+                return idGerente;
+        }
 
-        Long patioId // FK para Patio (único)
-) {}
+        public void setIdGerente(Long idGerente) {
+                this.idGerente = idGerente;
+        }
+
+        public String getNomeGerente() {
+                return nomeGerente;
+        }
+
+        public void setNomeGerente(String nomeGerente) {
+                this.nomeGerente = nomeGerente;
+        }
+
+        public String getTelefoneGerente() {
+                return telefoneGerente;
+        }
+
+        public void setTelefoneGerente(String telefoneGerente) {
+                this.telefoneGerente = telefoneGerente;
+        }
+
+        public String getCpfGerente() {
+                return cpfGerente;
+        }
+
+        public void setCpfGerente(String cpfGerente) {
+                this.cpfGerente = cpfGerente;
+        }
+
+        public Patio getPatio() {
+                return patio;
+        }
+
+        public void setPatio(Patio patio) {
+                this.patio = patio;
+        }
+}
