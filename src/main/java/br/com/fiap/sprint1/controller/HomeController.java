9eac304d43a691a55225a66ab72bb8c0613bd5e0
Giovanna Revito <giovannarev.roz@gmail.com>
2025-09-01 10:03:53 -0300

    adicionada dependencia thymeleaf para templates e estrutura html e css

---
+++ src/main/java/br/com/fiap/sprint1/controller/HomeController.java
@@ -0,0 +1,20 @@
+package br.com.fiap.sprint1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        // valores de exemplo, futuramente podem vir do banco de dados
+        model.addAttribute("totalMotos", 150);
+        model.addAttribute("vagasDisponiveis", 45);
+        model.addAttribute("notificacoes", 3);
+
+        return "home"; // procura em templates/home.html
+    }
+}
+
